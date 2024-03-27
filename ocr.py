54b4561fb6a93a19475a6a87f9058869738a3394
Rodney Finkel <111357994+RodneyFinkel@users.noreply.github.com>
2024-03-27 17:37:12 +0200

    pre func

--- ocr.py
+++ ocr.py
@@ -20,3 +20,18 @@ print(ocr_string)
 
 model = "HuggingFaceH4/zephyr-7b-alpha"
 pipe = pipeline("text-generation", model=model, torch_dtype=torch.bfloat16, device_map="auto" )
+
+
+# Each message can have 1 of 3 roles: "system" (to provide initial instructions), "user", or "assistant". For inference, make sure "user" is the role in the final message.
+messages = [
+    {
+        "role": "system",
+        "content": "You are a JSON converter which receives raw boarding pass OCR information as a string and returns a structured JSON output by organising the information in the string.",
+    },
+    {"role": "user", "content": f"Extract the name of the passenger, name of the airline, Flight number, City of Departure, City of Arrival, Date of Departure from this OCR data: {ocr_string}"},
+]
+# We use the tokenizer's chat template to format each message - see https://huggingface.co/docs/transformers/main/en/chat_templating
+
+prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+outputs = pipe(prompt, max_new_tokens=1000, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
+print(outputs[0]["generated_text"])
